66360aea626cee38fb2a3eb42dea48fcf9a725f8
Gosse Yannick Cyriaque GB <gosseyannick553@gmail.com>
2024-07-08 01:57:58 +0000

    refactor: improve the SLL search algorithm

--- linked-list/search-in-sll.py
+++ linked-list/search-in-sll.py
@@ -5,18 +5,9 @@ from Node import Node
 #return the updated linked list head after adding the target value at the end of the linked list.
 
 def search(head, value) -> bool:
-    if head is None:
-        return False
-    
-    current = head
-    if current.next is None and current.data != value:
-        return False
-    elif current.next is None and current.data == value:
-        return True
-    else:
-        while current.next:
-            if current.data == value:
-                return True
-            current = current.next
-        return False
+    while current.next:
+        if current.data == value:
+            return True
+        current = current.next
+    return False
     
