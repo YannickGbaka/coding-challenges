067ac59e7c29d5c2d3806893544aeb605f3d2442
Gosse Yannick Cyriaque GB <gosseyannick553@gmail.com>
2024-07-10 15:02:05 +0000

    refactor: improved cycle detection function

--- linked-list/detect-cycle.py
+++ linked-list/detect-cycle.py
@@ -16,3 +16,17 @@ def detect_cycle(head):
         current = current.next
     return False
 
+
+def detect_cycle2(head):
+    p1 = head
+    p2 = head
+    
+    while p1 and p2 and p2.next:
+        p1 = p1.next
+        p2 = p2.next.next
+        
+        if p1 == p2:
+            return True
+        
+    return False    
+    
