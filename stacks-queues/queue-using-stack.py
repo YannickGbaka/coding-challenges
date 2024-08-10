4d20b68aae1f6d626124557471d21149961ba617
Gosse Yannick Cyriaque GB <gosseyannick553@gmail.com>
2024-08-10 20:21:53 +0000

    refactor: implement queue using two stacks

--- stacks-queues/queue-using-stack.py
+++ stacks-queues/queue-using-stack.py
@@ -5,13 +5,21 @@ class NewQueue:
         self.main_stack = Stack()
         self.aux_stack = Stack()
     
-    def enqueue(self, data):
-        self.main_stack.push(data)
-    
-    def dequeue(self):
-        if self.aux_stack.is_empty():
+    def enqueue(self, value):
+        if self.main_stack.is_empty():
+            self.main_stack.push(value)
+        else:
             while not self.main_stack.is_empty():
                 self.aux_stack.push(self.main_stack.pop())
             
-        return self.aux_stack.pop()
+            self.main_stack.push(value)
+
+            while not self.aux_stack.is_empty():
+               self.main_stack.push(self.aux_stack.pop())
+
+
+    # Removes the element from the queue
+    def dequeue(self):
+        if not self.main_stack.is_empty():
+            return self.main_stack.pop()
             
